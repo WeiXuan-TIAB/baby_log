1285ffe77455f9ac5a7dcb50247a836168c414cf
TIAB <wbigbear82230@gmail.com>
2025-11-07 00:18:00 +0800

    基本routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { BabyListComponent } from '../pages/baby-list/baby-list.component';
+import { FeedFormComponent } from '../pages/feed-form/feed-form.component';
+import { DiaperFormComponent } from '../pages/diaper-form/diaper-form.component';
+import { SleepFormComponent } from '../pages/sleep-form/sleep-form.component';
+import { RecordListComponent } from '../pages/record-list.component.ts/record-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'babies', pathMatch: 'full' },
+  { path: 'babies', component: BabyListComponent, title: 'Babies' },
+  { path: 'feed', component: FeedFormComponent, title: 'Feed' },
+  { path: 'diaper', component: DiaperFormComponent, title: 'Diaper' },
+  { path: 'sleep', component: SleepFormComponent, title: 'Sleep' },
+  { path: 'records', component: RecordListComponent, title: 'Records' },
+  { path: '**', redirectTo: 'babies' },
+];
